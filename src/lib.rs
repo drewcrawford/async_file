d4800164a7cd9e93f4e38057349f4a9ce5e7a463
Drew Crawford <drew@sealedabstract.com>
2024-10-23 03:01:20 -0500

    Various read operations

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,18 @@ impl File {
     pub async fn open(path: impl AsRef<Path>) -> Result<Self,Error> {
         sys::File::open(path).await.map(File).map_err(Error)
     }
+    pub async fn read(&mut self, buf_size: usize) -> Result<(usize, Box<[u8]>), Error> {
+        self.0.read(buf_size).await.map_err(Error)
+    }
+
+    pub async fn seek(&mut self, pos: std::io::SeekFrom) -> Result<u64, Error> {
+        self.0.seek(pos).await.map_err(Error)
+    }
 }
 
 #[derive(Debug)]
+#[derive(thiserror::Error)]
+#[error("afile error {0}")]
 pub struct Error(sys::Error);
 
 
@@ -25,8 +34,29 @@ pub struct Error(sys::Error);
 
     #[test]
     fn test_open_file() {
+        logwise::context::Context::reset("test_open_file");
         test_executors::spin_on(async {
-            let file = File::open("/dev/zero").await.unwrap();
+            let _file = File::open("/dev/zero").await.unwrap();
+        });
+    }
+    #[test]
+    fn test_read_file() {
+        logwise::context::Context::reset("test_read_file");
+        test_executors::spin_on(async {
+            let mut file = File::open("/dev/zero").await.unwrap();
+            let (read, buf) = file.read(1024).await.unwrap();
+            assert_eq!(read, 1024);
+            assert_eq!(buf.len(), 1024);
+            assert_eq!(buf.iter().all(|&x| x == 0), true);
+        });
+    }
+
+    #[test]
+    fn test_seek_file() {
+        test_executors::spin_on(async {
+            let mut file = File::open("/dev/zero").await.unwrap();
+            let pos = file.seek(std::io::SeekFrom::Start(1024)).await.unwrap();
+            assert_eq!(pos, 1024);
         });
     }
 }
--- src/std_impl.rs
+++ src/std_impl.rs
@@ -1,16 +1,13 @@
-use std::future::Future;
 use std::io::Read;
-use std::os::unix::raw::mode_t;
 use std::path::Path;
-use std::sync::Arc;
 use blocking::unblock;
-
+use std::io::Seek;
 /**
 stdlib-based implementation*/
 
 pub struct File(Option<std::fs::File>);
 
-#[derive(Debug,thiserror::Error)]
+#[derive(Debug, thiserror::Error)]
 #[non_exhaustive]
 pub enum Error {
     #[error("I/O error: {0}")]
@@ -21,33 +18,48 @@ impl File {
     fn new(file: std::fs::File) -> Self {
         File(Some(file))
     }
-    pub async fn open(path: impl AsRef<Path>) -> Result<Self,Error> {
+    pub async fn open(path: impl AsRef<Path>) -> Result<Self, Error> {
         logwise::perfwarn_begin!("afile uses blocking on this platform");
         let path = path.as_ref().to_owned();
         unblock(|| std::fs::File::open(path)).await.map(File::new).map_err(|e| e.into())
     }
 
-    fn read(&mut self,buf_size: usize) -> impl Future<Output=Result<(usize,Box<[u8]>),Error>> + Send + use<'_> {
+    pub async fn read(&mut self, buf_size: usize) -> Result<(usize, Box<[u8]>), Error> {
         let mut move_file = self.0.take().expect("File operation in-flight already");
-        async move {
-            logwise::perfwarn_begin!("afile uses blocking on this platform");
-
-            unblock(move || {
-                let mut buf = vec![0; buf_size];
-                let read = move_file.read(&mut buf);
-                match read {
-                    Ok(read) => {
-                        Ok((move_file,read, buf.into_boxed_slice()))
-                    }
-                    Err(e) => {
-                        Err(e)
-                    }
+        logwise::perfwarn_begin!("afile uses blocking on this platform");
+        unblock(move || {
+            let mut buf = vec![0; buf_size];
+            let read = move_file.read(&mut buf);
+            match read {
+                Ok(read) => {
+                    Ok((move_file, read, buf.into_boxed_slice()))
                 }
-            }).await.map(|(file, read,buf)| {
-                self.0 = Some(file);
-                (read,buf)
-            }).map_err(|e| e.into())
-        }
+                Err(e) => {
+                    Err(e)
+                }
+            }
+        }).await.map(|(file, read, buf)| {
+            self.0 = Some(file);
+            (read, buf)
+        }).map_err(|e| e.into())
     }
 
+    pub async fn seek(&mut self, pos: std::io::SeekFrom) -> Result<u64, Error> {
+        let mut move_file = self.0.take().expect("File operation in-flight already");
+        logwise::perfwarn_begin!("afile uses blocking on this platform");
+        unblock(move || {
+            let pos = move_file.seek(pos);
+            match pos {
+                Ok(pos) => {
+                    Ok((move_file, pos))
+                }
+                Err(e) => {
+                    Err(e)
+                }
+            }
+        }).await.map(|(file, pos)| {
+            self.0 = Some(file);
+            pos
+        }).map_err(|e| e.into())
+    }
 }
